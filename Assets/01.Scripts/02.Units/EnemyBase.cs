59ad8f1b2bb19e0dc4385734d59fb89b585c818d
Teca <taewon4261@gmail.com>
2026-01-12 00:24:32 +0900

    chore / 웨이브 몬스터 수 몬스터 유닛 SO 데이터 수정
    밸런스 맞춰보는중

--- Assets/01.Scripts/02.Units/EnemyBase.cs
+++ Assets/01.Scripts/02.Units/EnemyBase.cs
@@ -98,90 +98,9 @@ public class EnemyBase : MonoBehaviour
         if (wall != null)
         {
             wall.TakeDamage(data.atk);
+            Die(false);
         }
-        Die(false);
        
     }
 }
 
-
-
-
-//[Header("Stats (나중에 SO로 연결 가능)")]
-//public float moveSpeed = 2f;
-//public int damage = 5;
-//public float attackInterval = 1f;
-//public float attackRange = 0.2f;
-
-//[Header("Grid")]
-//public Vector2Int currentGridPos; // 지금 적이 있는 칸(시작할 때 세팅)
-//private Tile currentTile;
-
-//private float nextAttackTime;
-//private UnitBase target;
-
-//private void Start()
-//{
-//    currentTile = GridManager.Instance.GetTile(currentGridPos);
-//    if (currentTile != null)
-//        transform.position = currentTile.transform.position;
-//}
-
-//private void Update()
-//{
-//    Tile nextTile = GetNextTile();
-
-//    // 끝(성 도착) 처리
-//    if (nextTile == null)
-//    {
-//        // TODO: 성 데미지 주기/클리어 등
-//        return;
-//    }
-
-//    // 막혔으면 공격
-//    if (nextTile.IsBlocked)
-//    {
-//        target = nextTile.occupiedUnit;
-//        AttackIfPossible();
-//        return;
-//    }
-
-//    // 안 막혔으면 이동
-//    target = null;
-//    MoveTo(nextTile);
-//}
-
-//Tile GetNextTile()
-//{
-//    // 오른쪽으로 전진
-//    Vector2Int nextPos = currentGridPos + Vector2Int.right;
-//    return GridManager.Instance.GetTile(nextPos);
-//}
-
-//void MoveTo(Tile nextTile)
-//{
-//    Vector3 dest = nextTile.transform.position;
-//    transform.position = Vector3.MoveTowards(transform.position, dest, moveSpeed * Time.deltaTime);
-
-//    if (Vector3.Distance(transform.position, dest) < 0.01f)
-//    {
-//        currentTile = nextTile;
-//        currentGridPos = currentTile.gridPos;
-//    }
-//}
-
-//void AttackIfPossible()
-//{
-//    if (target == null) return;
-
-//    float dist = Vector3.Distance(transform.position, target.transform.position);
-//    if (dist > attackRange) return;
-
-//    if (Time.time >= nextAttackTime)
-//    {
-//        nextAttackTime = Time.time + attackInterval;
-//        target.TakeDamage(damage);
-
-//        // 타겟이 죽어서 타일이 비워졌으면 다음 프레임부터 다시 전진함
-//    }
-//}
